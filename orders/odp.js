0637ec3dbb3368a13a69923c24220c6cb2412888
Sairam Sarpe <sairam.s18@iiits.in>
2026-09-21 19:37:05 +0530

    feat: Added new order function changes

--- orders/odp.js
+++ orders/odp.js
@@ -1,4 +1,9 @@
-function order()
+function order(orderID)
 {
-
+    console.log('Order ID is :' +orderID);
+    /* get order information */
+    fetch(orderID)
+    .then(result)
+    .catch(error)
+    .finally()
 }
